b45544e6b8a7fd411d652ef9e466f3c1b8cbc7b3
cbg1991 <christiangaillard@gmail.com>
2023-01-15 01:24:58 -0800

    finished assignment 5

--- assignment5.cpp
+++ assignment5.cpp
@@ -3,12 +3,13 @@
 #include <iostream>
 using namespace std;
 
-void stringPrint(string array[], const int SIZE);
-void salesPrint(int array[], const int SIZE);
+//prototype functions
 int averageSales(int array10[], const int SIZE);
 void aboveAverage(int salesArray[], string colorArray[], const int SIZE, int average);
+
 int main() 
 {
+    /* initalize and populate color array, initalize sales array, loop through color array while entering numbers into sales array */
 	const int SIZE = 6;
 	int monthlyAverage = 0;
 	string colors[SIZE] = {"purple", "blue", "green", "yellow", "orange", "red"};
@@ -17,26 +18,17 @@ int main()
 		cout << "Enter " << colors[i] << " monthly sales: ";
 		cin >> sales[i];
 	}
+
+    //call function to calc average and print, then calc colors above average
 	monthlyAverage = averageSales(sales, SIZE);
 	cout << "The average monthly sales is: " << monthlyAverage << "\n";
 
 	aboveAverage(sales, colors, SIZE, monthlyAverage);
-	//stringPrint(colors, SIZE);
+
 return 0;
 }
-void stringPrint(string array[], const int SIZE) 
-{
-	for(int i = 0; i < SIZE; i++) {
-		cout << array[i] << " ";
-	}
-}
 
-void salesPrint(int array[], const int SIZE) {
-	for(int i = 0; i < SIZE; i++) {
-		cout << array[i] << " ";
-	}
-}
-
+//loop through array adding value to sum, then divide sum by number of values
 int averageSales(int array[], const int SIZE) {
 	int average;
 	int sum;
@@ -47,11 +39,12 @@ int averageSales(int array[], const int SIZE) {
 	return average;
 }
 
+//loop through sales array, if value is greater or equal to average, print same index from color array
 void aboveAverage(int salesArray[], string colorArray[], const int SIZE, int average) {
 	cout << "Top selling: \n";
 	for(int i = 0; i < SIZE; i++) {
 		if(salesArray[i] >= average) {
-			cout << colorArray[i] << "\n";
+			cout << colorArray[i] << ": (" << salesArray[i] << ")\n";
 		}
 	}
 }
